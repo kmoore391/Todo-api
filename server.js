fc26ffc3fe210d55428a89589840581cd4d380d4
Kevin Moore <kevin391@msn.com>
2016-03-16 13:35:19 +0000

    update todos

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.post('/todos', function (req, res) {
 	res.json(todos);
 });
 
-//DELETE /todos
+//DELETE /todos/id
 app.delete('/todos/:id', function (req, res) {
 	var todoId  =  parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -63,6 +63,39 @@ app.delete('/todos/:id', function (req, res) {
 		res.status(404).json({"error": "no todo found with that id"});
 	}
 });
+
+//update /id
+app.put('/todos/:id', function (req, res) {
+	var body = _.pick(req.body, 'description', 'completed');
+	var validAttributes = {};
+
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if (!matchedTodo) {
+		return res.status(404).send();
+	}
+
+	if(body.hawOwnProperty('completed')) || !_.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	}else if (body.hawOwnProperty('completed')) {
+		return res.status(400).send();
+	}else {
+		//never provided
+	}
+
+	if(body.hawOwnProperty('description')) || !_.isString(body.description) || body.description.trim().length > 0 ){
+		validAttributes.description = body.description;
+	}else if (body.hawOwnProperty('description')) {
+		return res.status(400).send();
+	}else {
+		//never provided
+	}
+	
+	_.extend(matchedTodo,validAttributes);
+	
+});
+
 app.listen(PORT, function (){
 	console.log('Express listening on port ' + PORT + '!');
 });
