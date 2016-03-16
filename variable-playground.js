b6093d8a11e6bcd7718db6eead4a828e95894469
Kevin Moore <kevin391@msn.com>
2016-03-16 17:01:53 +0000

    Filtered Todos

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'Kevin',
+// 	age: 21
+// };
+
+// function updatePerson (obj) {
+// 	obj.age = 34;
+	
+// };
+
+//Array Example
+
+var grades = [34, 35, 46, 35];
+
+function updateGrade (grades) {
+ 	grades[1] = 1;
+};
+
+function addGrade (grades) {
+ 	grades.push(1);
+};
+
+updateGrade(grades);
+console.log(grades);
