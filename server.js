225e39d01526cc4333ee37747f28f8ce60872883
Kevin Moore <kevin391@msn.com>
2016-03-16 00:36:13 +0000

    Updated todos

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -38,7 +39,13 @@ app.get('/todos/:id', function (req, res) {
 
 //POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(404).send();
+	}
+	body.description = body.description.trim();
 	body.id = todoNextId++;
 	
 	todos.push(body)
