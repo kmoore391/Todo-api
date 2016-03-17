43114a77a51736634a9bbe7f63db7c01c4839a1a
Kevin Moore <kevin391@msn.com>
2016-03-17 09:39:46 +0000

    add todo model and POST Todo

--- serverPRESQLITE.js
+++ serverPRESQLITE.js
@@ -64,21 +64,17 @@ app.get('/todos/:id', function(req, res) {
 
 //POST /todos
 app.post('/todos', function(req, res) {
+
 	var body = _.pick(req.body, 'description', 'completed');
 
-	db.todo.create(body).then(function(todo) {
-		res.json(todo.toJSON());
-	}, function(e) {
-		res.status(400).json(e);
-	});
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(404).send();
-	// }
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return res.status(404).send();
+	}
+	body.description = body.description.trim();
+	body.id = todoNextId++;
 
-	// todos.push(body)
-	// res.json(todos);
+	todos.push(body)
+	res.json(todos);
 });
 
 //DELETE /todos/id
