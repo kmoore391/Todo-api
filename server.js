c09918f61f0a3434b2ac3fff25438f2de49a6e90
Kevin Moore <kevin391@msn.com>
2016-03-18 14:14:21 +0000

    delete by id

--- server.js
+++ server.js
@@ -30,40 +30,13 @@ app.get('/todos', function(req, res) {
 			$like: '%' + query.q + '%'
 		};
 	}
-	db.todo.findAll({where: where}).then(function (todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
 	}, function(e) {
 		res.status(500).send();
 	});
-
-
-
-	// var queryParams = req.query;
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {
-	// 		completed: false
-	// 	});
-	// };
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	//filteredTodos = _.where(filteredTodos, {description: queryParams.description});
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-
-	// };
-
-	// if (filteredTodos) {
-	// 	res.json(filteredTodos);
-	// } else {
-	// 	res.status(404).send();
-	// }
 
 });
 
@@ -94,22 +67,25 @@ app.post('/todos', function(req, res) {
 
 //DELETE /todos/id
 app.delete('/todos/:id', function(req, res) {
+
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0)  {
+			res.status(404).json({
+				error: 'No Todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+		
+	}, function(e) {
+		res.status(500).send();
 	});
-
-
-	if (matchedTodo) {
-		//res.json(matchedTodo);
-		//todos.splice(index, 1);	
-		todos = _.without(todos, matchedTodo);
-		res.json(todos);
-	} else {
-		res.status(404).json({
-			"error": "no todo found with that id"
-		});
-	}
+	
 });
 
 //update /id
