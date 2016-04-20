4a093b6929319474372387bf900c95254ebd7151
Kevin Moore <kevin391@msn.com>
2016-04-19 22:47:28 -0400

    login user token

--- server.js
+++ server.js
@@ -233,14 +233,14 @@ app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 
 	db.user.authenticate(body).then( function (user) {
-		res.json(user.toPublicJSON());
+		res.header('Auth', user.generateToken('authentication')).json(user.toPublicJSON());
 	}, function (){
 		res.status(401).send();
 	});
 });
 
 
-db.sequelize.sync({force: true}).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
